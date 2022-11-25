f77f7559cf0d556bc49edaa92e07144c96fdc674
Neil <homzzr@gmail.com>
2022-11-25 19:03:23 +0300

    Добавление лейбла с счетчиком и кнопки для запуска счетчика

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "Значение счетчика: \(number)"
     }
-
-
+    
+    @IBAction func clickButton(_ sender: Any) {
+        number += 1
+        counterLabel.text = "Значение счетчика: \(number)"
+    }
 }
 
