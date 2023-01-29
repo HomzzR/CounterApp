d085e941beb40ad4248965c6064f16a0564753c2
Neil <homzzr@gmail.com>
2023-01-29 18:53:48 +0300

    Добавил счетчик и сброс

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelView: UILabel!
+    var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelView.text = "Значение счётчика: \(score)"
     }
 
 
+    @IBAction func resetButton(_ sender: UIButton) {
+        score = 0
+        labelView.text = "Значение счётчика: \(score)"
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        score += 1
+        labelView.text = "Значение счётчика: \(score)"
+    }
 }
 
